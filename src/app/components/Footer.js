0a4c2f7ef4e16b5da856422fee4aae9ea01b2471
Hengki Zulfikri <hengkizulfikri47540@gmail.com>
2025-11-01 20:34:25 +0700

    initial setup

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-sm text-center italic'>
+    <p>&copy; 2025 XenFace. Ramalan ini berdasarkan mode AI vision, 
+        bukan sepenuhnya untuk dipercaya.</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/app/components/Navheader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navheader() {
+  return (
+    <header className="text-center">
+        <h1 className="text-4xl font-extrabold bg-gradient-to-r from-red-600 via-orange-700 to-yellow-400
+        text-transparent bg-clip-text shadow-lg inline-block" style={{ filter: 'drop-shadow(0 0 10px rgba(150, 50, 200, 0.7))' }}>
+            CUYFACE | PREDICT YOUR FACE</h1>
+        <p className="text-gray-300">Ramalan berdasarkan foto selfie kamu</p>
+    </header>
+  )
+}
+
+export default Navheader
